1cf5b6028a85ae1bb76667f4da3e30fdfe40cd37
Zara Afridi <zara,afridi@mail.utoronto.ca>
2020-02-02 10:33:34 -0500

    final extension logic

--- extension/background.js
+++ extension/background.js
@@ -1,14 +1,16 @@
 // background.js
 
-// Called when the user clicks on the browser action.
-// chrome.browserAction.onClicked.addListener(function() {
-//     // chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-//     //     chrome.tabs.sendMessage(tabs[0].id,"toggle");
-//     // })
-//   window.open("popup.html", "extension_popup", "width=300,height=400,status=no,scrollbars=yes,resizable=no");
-
   
-//  });
+
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function() {
+    // chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    //     chrome.tabs.sendMessage(tabs[0].id,"toggle");
+    // })
+  window.open("popup.html", "extension_popup", "width=300,height=400,status=no,scrollbars=yes,resizable=no");
+  
+
+  });
 
 // **** trying to do it without clicking **///
 
@@ -21,9 +23,9 @@
 //     }
 // }});
 
-chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    var url = tabs[0].url;
-    if (url.includes('google.ca/maps/dir')) {
-        alert("YOOOOO");
-    }
-});
+// chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+//     var url = tabs[0].url;
+//     if (url.includes('google.ca/maps/dir')) {
+//         alert("YOOOOO");
+//     }
+// });
--- extension/script.js
+++ extension/script.js
@@ -1,14 +1,3 @@
-var request = require('request');
+import request from 'request';
 
-request({
-  uri: 'https://www.parsehub.com/api/v2/runs/t64TM1uf4VVO/data',
-  method: 'GET',
-  gzip: true,
-  qs: {
-    api_key: "tnA3rUBGCd_r",
-    format: "JSON"
-  }
-}, function(err, resp, body) {
-  console.log(body);
-});
-
+var myRequest = new Request(input[, init]);
