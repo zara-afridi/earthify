7870b6cba64d4e5a5570c31513732c58f6ad280d
Zara Afridi <zara,afridi@mail.utoronto.ca>
2020-02-02 03:20:15 -0500

    updated extension functionality

--- extension/background.js
+++ extension/background.js
@@ -1,23 +1,29 @@
 // background.js
 
 // Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.create({'url': chrome.extension.getURL('mainPage.html')}, function(tab) {
-        // Tab opened.
-  });
+// chrome.browserAction.onClicked.addListener(function() {
+//     // chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+//     //     chrome.tabs.sendMessage(tabs[0].id,"toggle");
+//     // })
+//   window.open("popup.html", "extension_popup", "width=300,height=400,status=no,scrollbars=yes,resizable=no");
+
+  
+//  });
+
+// **** trying to do it without clicking **///
+
+// chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+//     if (changeInfo.status == 'complete' && tab.active) {
+//         if (tab.url.includes('google.ca/maps/dir')){
+//            // $("body").append("<p> TEST </p>")
+//             $('#dialogDiv').dialog('open');
+//            alert(tabs.url);
+//     }
+// }});
+
+chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    var url = tabs[0].url;
+    if (url.includes('google.ca/maps/dir')) {
+        alert("YOOOOO");
+    }
 });
-
-// do things when on maps page
-chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    // Replace the current rules
-      chrome.declarativeContent.onPageChanged.addRules([
-      {
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostEquals : 'www.google.com', schemes: ['https']},
-          })
-        ], 
-        actions: [new chrome.declarativeContent.ShowPageAction()]
-      }
-     ])
-   })
-
--- extension/content.js
+++ extension/content.js
@@ -1,11 +1,25 @@
-// content.js
-// chrome.runtime.onMessage.addListener(
-//     function(request, sender, sendResponse) {
-//       if( request.message === "clicked_browser_action" ) {
-//         var firstHref = $("a[href^='http']").eq(0).attr("href");
-//         alert("hello!");
-//         console.log(firstHref);
-//       }
+
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+    if (changeInfo.status == 'complete' && tab.active) {
+        window.open("popup.html", "extension_popup", 
+        width=300,height=400,status=no,scrollbars=yes,resizable=no,top=100,left=100);
+}});
+
+
+
+// open a dialog (simulating a pop up) when user searches directions
+// jQuery("#dialogDiv").dialog({
+//     autoOpen: false, 
+//     draggable: true,
+//     resizable: false,
+//     height: 'auto',
+//     width: 500,
+//     modal: false,
+//     open: function(event, ui) {
+//         $(event.target).parent().css('position', 'fixed');
+//         $(event.target).parent().css('top', '5px');
+//         $(event.target).parent().css('left', '10px');
 //     }
-//   );
+
+// });
   
